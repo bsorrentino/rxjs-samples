3bd4406d2793f9a58cf63e19466b8b862a929ba3
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-03-24 18:15:07 +0100

    update

--- app/autocomplete/autocomplete.ts
+++ app/autocomplete/autocomplete.ts
@@ -4,49 +4,50 @@ import $ from 'jquery';
 
 // Search Wikipedia for a given term
 function searchWikipedia (term) {
-return $.ajax({
-    url: 'http://en.wikipedia.org/w/api.php',
-    dataType: 'jsonp',
-    data: {
-    action: 'opensearch',
-    format: 'json',
-    search: term
-    }
-}).promise();
+    return $.ajax({
+        url: 'http://en.wikipedia.org/w/api.php',
+        dataType: 'jsonp',
+        data: {
+        action: 'opensearch',
+        format: 'json',
+        search: term
+        }
+    }).promise();
 }
 
 function main() {
 
-console.log( "MAIN");
+    console.log( "MAIN");
 
-var $input = $('#textInput'),
-    $results = $('#results');
+    var $input = $('#textInput'),
+        $results = $('#results');
 
-// Get all distinct key up events from the input and only fire if long enough and distinct
-var keyup = Rx.Observable.fromEvent($input, 'keyup')
-    .map( (e) => {
-        return e.target.value; // Project the text from the input
-    })
-    .filter( (text) => {
-        return text.length > 2; // Only if the text is longer than 2 characters
-    })
-    .debounceTime(750 /* Pause for 750ms */ )
-    .distinctUntilChanged(); // Only if the value has changed
+    // Get all distinct key up events from the input and only fire if long enough and distinct
+    var keyup = Rx.Observable.fromEvent($input, 'keyup')
+        .map( (e) => {
+            return e.target.value; // Project the text from the input
+        })
+        .filter( (text) => {
+            return text.length > 2; // Only if the text is longer than 2 characters
+        })
+        .debounceTime(750 /* Pause for 750ms */ )
+        .distinctUntilChanged(); // Only if the value has changed
 
-var searcher = keyup.switchMap(searchWikipedia);
-
-searcher.subscribe(
-    (data) => {
-        $results
-            .empty()
-            .append ($.map(data[1], function (v) { return $('<li>').text(v); }));
-    },
-    (error) => {
-        $results
-            .empty()
-            .append($('<li>'))
-            .text('Error:' + error);
-    });
+    keyup.switchMap(searchWikipedia)
+            .subscribe(
+                (data) => {
+                    $results
+                        .empty()
+                        .append ($.map(data[1], (v) => { 
+                            return $('<li>').text(v); 
+                        }));
+                },
+                (error) => {
+                    $results
+                        .empty()
+                        .append($('<li>'))
+                        .text('Error:' + error);
+                });
 }
 
 main(); 
