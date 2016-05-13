d4f2a8df5936120fa6d1e1666a0c178fe37db167
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-05-13 22:17:11 +0200

    add map operator

--- app/autocomplete.ts
+++ app/autocomplete.ts
@@ -16,14 +16,19 @@ function main() {
      
      -keyup1--keyup12---keyup13----keyup14---keyup15----->
          |       |         |          |         |
-     ---------------------------------------------------->
+                   MAP( event => value )
+         |       |         |          |         |
+     ---v1-------v2--------v3---------v3--------v4------->
      
      */
     Rx.Observable.fromEvent($input, 'keyup')
+        .map( (e:Event) => {
+            return e.target['value']; // Project the text from the input
+        })
         .subscribe(
             (e:Event) => {
                 console.log( "event", e );
-            });
+        });
 }
 
 main();
