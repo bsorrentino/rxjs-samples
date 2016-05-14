d97b4d161b2f55bf583959ed627ae8f71e42baad
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-05-14 15:51:04 +0200

    update

--- app/autocomplete.ts
+++ app/autocomplete.ts
@@ -13,7 +13,7 @@ function main() {
     var $input = $('#textInput');
 
     /**
-     
+
      -keyup1--keyup12---keyup13----keyup14---keyup15----->
          |       |         |          |         |
                    MAP( event => value )
@@ -22,15 +22,15 @@ function main() {
                    FILTER( condition )
                  |         |                    |
      ------------v2--------v3-------------------v4------->
-     
+
      */
     Rx.Observable.fromEvent($input, 'keyup')
         .map( (e:Event) => {
-            return e.target['value']; // Project the text from the input
+            return e.target['value']; // map event to input text
         })
         .filter( (text:string) => {
             return text.length > 2; // Only if the text is longer than 2 characters
-        })        
+        })
         .subscribe(
             (e:Event) => {
                 console.log( "event", e );
