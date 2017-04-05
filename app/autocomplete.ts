e7b412a6fb1ed24553273644fe35cdd1498210aa
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-04-05 16:59:54 +0200

    using only function

--- app/autocomplete.ts
+++ app/autocomplete.ts
@@ -4,59 +4,60 @@
 import * as Rx from "../jspm_packages/npm/rxjs@5.0.0-beta.3/Rx";
 import $  from "jquery";
 
+/**
+ * Search Wikipedia for a given term
+ *
+ */
+function rxSearch(term:string, lastRequest:{xhr:JQueryXHR} ):Rx.Observable<any> {
 
-class Wikipedia  {
-
-    private xhr:JQueryXHR;
-
-    private cancel() {
-        if( this.xhr != null ) {
-            this.xhr.abort();
-            console.log( "canceled!" );
-            this.xhr = null;
+    function cancel() {
+        if( lastRequest.xhr!=null && !lastRequest.xhr.status) {
+          lastRequest.xhr.abort();
+          console.log( "canceled!" );
+          lastRequest.xhr = null;
         }
     }
 
-    // Search Wikipedia for a given term
-    rxSearch(term:string):Rx.Observable<any> {
+    return Rx.Observable.create( (observer:Rx.Observer<any>) => {
 
-        return Rx.Observable.create( (observer:Rx.Observer<any>) => {
+        cancel();
 
-            this.cancel();
+        lastRequest.xhr = $.ajax({
+                url: '/proxy/en.wikipedia.org/w/api.php',
+                async:true,
+                data: {
+                    action: 'opensearch',
+                    format: 'json',
+                    search: term
+                },
+                error: (jqXHR: JQueryXHR, textStatus: string, errorThrown: string) => {
+                      observer.error( errorThrown );
+                },
+                success: (data: any, textStatus: string, jqXHR: JQueryXHR) => {
+                    observer.next( data );
+                    observer.complete();
+                }
+        });
+        return () => { // On Unsubscribe
+            cancel();
+        }
+    });
 
-            this.xhr = $.ajax({
-                    url: '/proxy/en.wikipedia.org/w/api.php',
-                    async:true,
-                    data: {
-                        action: 'opensearch',
-                        format: 'json',
-                        search: term
-                    },
-                    error: (jqXHR: JQueryXHR, textStatus: string, errorThrown: string) => {
-                        observer.error( errorThrown );
-                    },
-                    success: (data: any, textStatus: string, jqXHR: JQueryXHR) => {
-                        observer.next( data );
-                        observer.complete();
-                    }
-            });
-            return () => { // On Unsubscribe
-                this.cancel();
-            }
-        })
+  }
+
+/**
+ * Search Wikipedia for a given term and retry on failure
+ *
+ */
+function rxSearchAndRetry( retryFor:number, term:string, lastRequest:{xhr:JQueryXHR} ):Rx.Observable<any> {
+
+  return rxSearch( term, lastRequest )
         .retryWhen( (errors: Rx.Observable<any>) => {
-            return errors.scan( (errorCount:number, err:any) => {
-                                return errorCount + 1;
-                        }, 0)
-                        .takeWhile((errorCount) => {
-                            return errorCount < 5;
-                        })
+            return errors.scan( (errorCount:number, err:any) => errorCount + 1, 0)
+                        .takeWhile((errorCount) => errorCount < retryFor )
                         .delay(1000);
                     })
                     ;
-
-    }
-
 }
 
 function main() {
@@ -64,10 +65,13 @@ function main() {
     console.log( "STEP8");
 
     var $input = $('#textInput'),
-        $results = $('#results'),
-        wikipedia = new Wikipedia()
+        $results = $('#results')
         ;
-    const DEBOUNCE_TIME = 750;
+    const DEBOUNCE_TIME = 50;
+
+    let lastXHR = {
+      xhr:null
+    }
 
     // Get all distinct key up events from the input and only fire if long enough and distinct
     Rx.Observable.fromEvent($input, 'keyup')
@@ -75,7 +79,7 @@ function main() {
         .filter( (text:string) => text.length > 2)
         .debounceTime(DEBOUNCE_TIME)
         .distinctUntilChanged() // Only if the value has changed
-        .switchMap( (term:string) => wikipedia.rxSearch(term) )
+        .switchMap( (term:string) => rxSearch( term, lastXHR ) )
         .subscribe(
             (data:any) => {
                 $results
